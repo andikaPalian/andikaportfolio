341c30bc6e59b8d831a44f7e6b81981ef92152d6
Andika Palian <andika12des@gmail.com>
2025-07-06 15:48:54 +0800

    Update Contact button link in Home component to navigate to the Contact section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -51,7 +51,7 @@ const Home = () => {
                 View Projects
               </button>
             </a>
-            <a href="">
+            <a href="#contact">
               <button className="rounded-md border-2 border-dashed border-white bg-black px-6 py-[9px] font-semibold uppercase text-white transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_white] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
                 Contact Me
               </button>
